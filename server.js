dc7bf279c8ecccdc63ec9bd5f425b91e46e8f0ce
haimyno <haimyno@gmail.com>
2025-12-05 13:59:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 
 app.get('/', (req, res) => {
-  res.send('🚀 CI/CD Demo App Deployed via Dokploy!');
+  res.send('🚀 CI/CD Demo App Deployed via Dokploy!. This is my first CI/CD');
 });
 
 app.listen(PORT, () => console.log(`My Server is running on port ${PORT}   purpose`));
